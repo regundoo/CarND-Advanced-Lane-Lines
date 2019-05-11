023ac6aa4c4c83877e3fe0da02341d251e6a2de7
regundoo <4BV-fap-ewG-ytY>
2019-05-11 09:09:17 +0200

    first test

--- line_finder.py
+++ line_finder.py
@@ -5,5 +5,18 @@ Created on Tue May  7 12:52:08 2019
 @author: Y1PGLOCK
 """
 
-import calibration
+#importing some useful packages
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+import math
+import os
 
+
+#reading in an image
+image = mpimg.imread('test_images/test1.jpg')
+
+#printing out some stats and plotting
+print('This image is:', type(image), 'with dimensions:', image.shape)
+plt.imshow(image)  # if you wanted to show a single color channel image called 'gray', for example, call as plt.imshow(gray, cmap='gray')
