c49c521b036484d326cec1d334e1a6d751a4e9d6
regundoo <4BV-fap-ewG-ytY>
2019-05-19 11:25:21 +0200

    This is the working copy

--- SimpleLineFinder.py
+++ SimpleLineFinder.py
@@ -3,24 +3,45 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+import os
 
 from moviepy.editor import VideoFileClip
 from PerTransformer import PerTransformerClass
+from CameraCalibration import CalibrateCamera
 
-def undistort_image(dist_img):
+#Remove distortion from images
+def undistort_image(dist_img, show=False):
+    # Read camera mtx and dist from file and safe in variable.
     dist_pickel = pickle.load(open("./camera_cal/calibration_pickle.p", "rb"))
     mtx = dist_pickel["mtx"]
     dist = dist_pickel["dist"]
 
+    # undist. the Image with given values
     undist_img = cv2.undistort(dist_img, mtx, dist, None, mtx)
 
-    return undist_img
+    if show:
+        f, (ax1, ax2) = plt.subplots(1, 2, figsize=(9,6))
+        ax1.imshow(cv2.cvtColor(dist_img, cv2.COLOR_BGR2RGB))
+        ax1.set_title('Original Image', fontsize=20)
+        ax2.imshow(cv2.cvtColor(undist_img, cv2.COLOR_BGR2RGB))
+        ax2.set_title('Undistorted Image', fontsize=20)
+        return dist_img
+
+    else:
+        return undist_img
+
 
 def binary_threshold(img, s_thresh, sx_thresh):
     out_img = np.copy(img)
     # Convert to HLS color space and separate the S channel
-    hls = cv2.cvtColor(out_img, cv2.COLOR_RGB2HLS).astype(np.float)
+    hls = cv2.cvtColor(out_img, cv2.COLOR_RGB2HLS)
+    hls = cv2.medianBlur(hls, 5)
     s_channel = hls[:, :, 2]
+
+    b_channel = cv2.cvtColor(out_img, cv2.COLOR_RGB2Lab)
+    b_channel = b_channel[:, :, 2]
+
+
     # Sobel x
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY).astype(np.float)
     sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)  # Take the derivative in x
@@ -44,47 +65,13 @@ def binary_threshold(img, s_thresh, sx_thresh):
 
     return color_binary, combined_binary
 
-
-src = np.float32([[200, 720], [1125, 720], [685, 450], [595, 450]])
-dst = np.float32([[320, 720], [1000, 720], [1000, 0], [320, 0]])
-
-fname = "./output_images/straight_lines1.jpg"
-src_img = mpimg.imread(fname)
-
-src_pts = src.reshape((-1, 1, 2)).astype("int32")
-dst_pts = dst.reshape((-1, 1, 2)).astype("int32")
-cv2.polylines(src_img, [src_pts], True, (0, 255, 255), thickness=5)
-cv2.polylines(dst_img, [dst_pts], True, (255, 0, 255), thickness=5)
-
-plt.figure(figsize=(20, 10))
-plt.subplot(1, 2, 1)
-plt.title('src image')
-plt.imshow(src_img)
-plt.subplot(1, 2, 2)
-plt.title('warped image')
-plt.imshow(dst_img)
-
-binary_warped = warp(combined_binary, src, dst, False)
-
-plt.figure(figsize=(20, 10))
-plt.subplot(1, 2, 1)
-plt.title('thresholded image')
-plt.imshow(combined_binary, cmap='gray')
-plt.subplot(1, 2, 2)
-plt.title('warped thresholded image')
-plt.imshow(binary_warped, cmap='gray')
-plt.show()
-
-histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
-plt.plot(histogram)
-
-
 def sliding_window_search(img):
     # Create an output image to draw on and  visualize the result
     out_img = np.dstack((img, img, img)) * 255
     # Find the peak of the left and right halves of the histogram
     # These will be the starting point for the left and right lines
     histogram = np.sum(img[img.shape[0] // 2:, :], axis=0)
+    # Find the midle of the histogram
     midpoint = np.int(histogram.shape[0] / 2)
     left_bound = 150
     right_bound = 1150
@@ -93,7 +80,7 @@ def sliding_window_search(img):
     # print(leftx_base, rightx_base, midpoint)
 
     # Choose the number of sliding windows
-    nwindows = 9
+    nwindows = 16
     # Set height of windows
     window_height = np.int(img.shape[0] / nwindows)
     # Identify the x and y positions of all nonzero pixels in the image
@@ -129,9 +116,9 @@ def sliding_window_search(img):
         cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), (0, 255, 0), 2)
         # Identify the nonzero pixels in x and y within the window
         good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (
-                    nonzerox < win_xleft_high)).nonzero()[0]
+                nonzerox < win_xleft_high)).nonzero()[0]
         good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (
-                    nonzerox < win_xright_high)).nonzero()[0]
+                nonzerox < win_xright_high)).nonzero()[0]
         # Append these indices to the lists
         # print(good_left_inds)
         left_lane_inds.append(good_left_inds)
@@ -171,32 +158,28 @@ def sliding_window_search(img):
     return left_fit, right_fit, out_img
 
 
-# Generate x and y values for plotting
-ploty = np.linspace(0, out_img.shape[0] - 1, out_img.shape[0])
-left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
-right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
 
-plt.imshow(out_img)
-plt.plot(left_fitx, ploty, color='yellow')
-plt.plot(right_fitx, ploty, color='yellow')
-plt.xlim(0, 1280)
-plt.ylim(720, 0)
+def get_curverad(left_fit, right_fit, out_img):
 
+    lane_px_height = 275 # Manuel Value
+    lane_px_width = 413  # Manuel Value
 
-def get_curverad(left_fit, right_fit):
-    # Define conversions in x and y from pixels space to meters
-    ym_per_pix = 30 / 720  # meters per pixel in y dimension
-    xm_per_pix = 3.7 / 700  # meters per pixel in x dimension
+    ym_per_pix = (3./lane_px_height)  # meters per pixel in y dimension
+    xm_per_pix = (3.7/lane_px_width)  # meters per pixel in x dimension
 
     ploty = np.linspace(0, out_img.shape[0] - 1, out_img.shape[0])
     y_eval = np.max(ploty)
+
+
     leftx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
     rightx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
 
-    # Fit new polynomials to x,y in world space
+
     left_fit_cr = np.polyfit(ploty * ym_per_pix, leftx * xm_per_pix, 2)
     right_fit_cr = np.polyfit(ploty * ym_per_pix, rightx * xm_per_pix, 2)
-    # Calculate the new radii of curvature
+
+
+    # Calculate the new radius of curvature
     left_curverad = ((1 + (2 * left_fit_cr[0] * y_eval * ym_per_pix + left_fit_cr[1]) ** 2) ** 1.5) / np.absolute(
         2 * left_fit_cr[0])
     right_curverad = ((1 + (2 * right_fit_cr[0] * y_eval * ym_per_pix + right_fit_cr[1]) ** 2) ** 1.5) / np.absolute(
@@ -205,10 +188,6 @@ def get_curverad(left_fit, right_fit):
 
     return curverad
 
-
-curverad = get_curverad(left_fit, right_fit)
-# Now our radius of curvature is in meters
-print(curverad, 'm')
 
 
 def get_offset(left_fit, right_fit):
@@ -227,12 +206,8 @@ def get_offset(left_fit, right_fit):
     return offset
 
 
-offset = get_offset(left_fit, right_fit)
-# Now our radius of curvature is in meters
-print(offset, 'm')
 
-
-def draw_lines(warped, undist, left_fit, right_fit):
+def draw_lines(warped, undist, left_fit, right_fit,src ,dst, out_img, count):
     # Create an image to draw the lines on
     warp_zero = np.zeros_like(warped).astype(np.uint8)
     color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
@@ -249,28 +224,46 @@ def draw_lines(warped, undist, left_fit, right_fit):
     # Draw the lane onto the warped blank image
     cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
 
-    newwarp = warp(color_warp, src, dst, True)
+
+    newwarp = PerTransformerClass(src, dst).inverse_transform(color_warp)
     # Combine the result with the original image
     result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
 
-    curverad = get_curverad(left_fit, right_fit)
+    curverad = get_curverad(left_fit, right_fit, out_img)
     curvature_text = "Curvature: " + str(np.round(curverad, 2))
-    font = cv2.FONT_HERSHEY_COMPLEX
+    font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(result, curvature_text, (30, 60), font, 1, (0, 255, 0), 2)
 
     offset = get_offset(left_fit, right_fit)
-    offset_text = "Lane offset from center = {:.2f} m".format(offset)
-    font = cv2.FONT_HERSHEY_COMPLEX
+    offset_text = "Lane offset  = {:.2f} m".format(offset)
+    font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(result, offset_text, (30, 90), font, 1, (0, 255, 0), 2)
+
+    cv2.putText(result,'%4d' % count,(5,700), font, 1,(255,255,255),2,cv2.LINE_AA)
 
     return result
 
 
-result = draw_lines(binary_warped, undist_img, left_fit, right_fit)
-plt.imshow(result)
 
 last_left_fit = None
 last_right_fit = None
+
+def calc_warp_points():
+    src = np.float32([
+        [220, 651],
+        [350, 577],
+        [828, 577],
+        [921, 651]
+    ])
+
+    dst = np.float32([
+        [220, 651],
+        [220, 577],
+        [921, 577],
+        [921, 651]
+    ])
+    return src, dst
+
 
 
 def process_image(image):
@@ -280,30 +273,65 @@ def process_image(image):
     global last_left_fit, last_right_fit
     alpha = 0.2
 
-    undistorted = undistort_image(image)
-    
-    s_thresh=(120, 255)
-    sx_thresh=(20, 100)
-    color_binary, combined_binary = binary_threshold(undistorted, s_thresh, sx_thresh)
+    count = 0
+    font = cv2.FONT_HERSHEY_SIMPLEX
 
-    src = np.float32([[200, 720], [1125, 720], [685, 450], [595, 450]])
-    dst = np.float32([[320, 720], [1000, 720], [1000, 0], [320, 0]])
-    
-    binary_warped = PerTransformerClass(src, dst)
-    binary_warped = binary_warped.transform(combined_binary)
+    exists = os.path.isfile("./camera_cal/calibration_pickle.p")
 
-    left_fit, right_fit, _ = sliding_window_search(binary_warped)
+    if exists:
+        undistorted = undistort_image(image)
 
-    if last_left_fit is not None:
-        left_fit = (1 - alpha) * last_left_fit + alpha * left_fit
-        right_fit = last_right_fit * (1 - alpha) + right_fit * alpha
+        s_thresh = (120, 255)
+        sx_thresh = (20, 100)
+        color_binary, combined_binary = binary_threshold(undistorted, s_thresh, sx_thresh)
 
-    last_left_fit = left_fit
-    last_right_fit = right_fit
+        src, dst = calc_warp_points()
 
-    result = draw_lines(binary_warped, undistorted, left_fit, right_fit)
+        binary_warped = PerTransformerClass(src, dst)
+        binary_warped = binary_warped.transform(combined_binary)
 
-    return result
+        left_fit, right_fit, out_img = sliding_window_search(binary_warped)
+
+        if last_left_fit is not None:
+            left_fit = (1 - alpha) * last_left_fit + alpha * left_fit
+            right_fit = last_right_fit * (1 - alpha) + right_fit * alpha
+
+        last_left_fit = left_fit
+        last_right_fit = right_fit
+
+        result = draw_lines(binary_warped, undistorted, left_fit, right_fit, src, dst, out_img, count)
+
+
+        return result
+    else:
+        CalibrateCamera()
+
+        undistorted = undistort_image(image)
+
+        s_thresh = (180, 255)
+        sx_thresh = (20, 50)
+        color_binary, combined_binary = binary_threshold(undistorted, s_thresh, sx_thresh)
+
+        src, dst = calc_warp_points()
+
+        binary_warped = PerTransformerClass(src, dst)
+        binary_warped = binary_warped.transform(combined_binary)
+
+        left_fit, right_fit, out_img = sliding_window_search(binary_warped)
+
+        if last_left_fit is not None:
+            left_fit = (1 - alpha) * last_left_fit + alpha * left_fit
+            right_fit = last_right_fit * (1 - alpha) + right_fit * alpha
+
+        last_left_fit = left_fit
+        last_right_fit = right_fit
+
+        result = draw_lines(binary_warped, undistorted, left_fit, right_fit, src, dst, out_img, count)
+
+        return result
+
+
+
 
 # Run on a test image
 img = cv2.imread("test_images/test6.jpg")
@@ -311,11 +339,12 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 result = process_image(img)
 
-plt.figure(figsize=(16,8))
+plt.figure(figsize=(16, 8))
 plt.imshow(result)
+plt.show()
 plt.axis("off");
 
-#video_output = "output_images/project_video.mp4"
-#clip1 = VideoFileClip("project_video.mp4", audio=False)
-#clip1_output = clip1.fl_image(process_image) #NOTE: this function expects color images!!
-#clip1_output.write_videofile(video_output, audio=False)
+video_output = "output_images/project_video_hard.mp4"
+clip1 = VideoFileClip("harder_challenge_video.mp4", audio=False)
+clip1_output = clip1.fl_image(process_image)
+clip1_output.write_videofile(video_output, audio=False)
