6002b166f3adbed52776162c9c817074a75a29db
Patrick G <p.glocker@posteo.de>
2019-05-21 06:35:09 +0200

    Final update

--- CameraCalibration.py
+++ CameraCalibration.py
@@ -9,7 +9,7 @@ def calibrateCamera():
     nx = 6
     ny = 9
 
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    # prepare object points
     objp = np.zeros((nx * ny, 3), np.float32)
     objp[:, :2] = np.mgrid[0:ny, 0:nx].T.reshape(-1, 2)
 
@@ -27,19 +27,20 @@ def calibrateCamera():
         # Find the chessboard corners
         ret, corners = cv2.findChessboardCorners(gray, (ny, nx), None)
 
-        # If found, add object points, image points
+        # If found add object points image points
         if ret:
             print('points found in: ' + fname)
             objpoints.append(objp)
             imgpoints.append(corners)
 
-            # Draw and display the corners
+            # Draw corners
             # img = cv2.drawChessboardCorners(img, (ny, nx), corners, ret)
             output_fname = './camera_cal/found/' + fname.split('\\')[-1]
             print('Write ChessboardCorners to: ' + output_fname)
 
     img = cv2.imread(images[0])
     img_size = (img.shape[1], img.shape[0])
+    # safe the camera calibration matrix and the distorbtion factor to the pickle file
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
 
     dist_pickle = {}
